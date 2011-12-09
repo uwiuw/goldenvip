2b14b39584b27ff571604e65fff49d5e7b0b3e81
aceng <aceng.nursamsudin@gmail.com>
2011-12-09 13:35:40 -0800

    yg belum k upload
    Signed-off-by: aceng <aceng.nursamsudin@gmail.com>

--- application/libraries/business.php
+++ application/libraries/business.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
+ *  To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
     class business()
